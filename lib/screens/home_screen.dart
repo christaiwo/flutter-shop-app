19c7abecb603946a653ea8756a014abfd0d360c2
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2024-01-27 17:03:07 +0100

    update system

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,7 +17,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     selectedFilter = filters[0];
   }
